d86f30c6cfca78eaa4f61564f2541ecd07ec84e6
shujath-git900 <shujath.devops@gmail.com>
2026-04-26 14:34:52 +0530

    exit status function added

--- common.sh
+++ common.sh
@@ -1,3 +1,11 @@
 headings(){
   echo -e "\e[33m$1\e[0m"
 }
+
+status_check(){
+  if [ $? -eq 0 ]; then
+      echo -e "\e[32mSuccess\e[0m"
+  else
+    echo -e "\e[31mFailure\e[0m"
+  fi
+}
--- frontend.sh
+++ frontend.sh
@@ -2,37 +2,49 @@ source common.sh
 
 headings "Disable Nginx"
 dnf module disable nginx -y &>>OUTPUT.log
+status_check $?
 
 headings "Enable Nginx"
 dnf module enable nginx:1.26 -y &>>OUTPUT.log
+status_check $?
 
 headings "Install Nginx"
 dnf install -y nginx &>>OUTPUT.log
+status_check $?
+
 
 headings "copy nginx configuration"
 cp ngnix.conf /etc/nginx/nginx.conf &>>OUTPUT.log
+status_check $?
 
 headings "Install Nodejs 22 repo"
 curl -fsSL https://rpm.nodesource.com/setup_22.x | bash - &>>OUTPUT.log
+status_check $?
 
 headings "install nodejs"
 dnf install -y nodejs &>>OUTPUT.log
+status_check $?
 
 headings "Nodejs version"
 node --version &>>OUTPUT.log
+status_check $?
 
 headings "npm version"
 npm --version &>>OUTPUT.log
+status_check $?
 
 headings "Download Application"
 curl -L -o /tmp/frontend.tar.gz https://raw.githubusercontent.com/raghudevopsb88/wealth-project/main/artifacts/frontend.tar.gz &>>OUTPUT.log
+status_check $?
 
 headings "Create frontend dir"
 mkdir -p /tmp/frontend &>>OUTPUT.log
+status_check $?
 
 headings "Change and Extract appl in frontend dir"
 cd /tmp/frontend
 tar xzf /tmp/frontend.tar.gz &>>OUTPUT.log
+status_check $?
 
 
 cd /tmp/frontend
@@ -40,15 +52,20 @@ cd /tmp/frontend
 headings "build application"
 npm ci &>>OUTPUT.log
 npm run build &>>OUTPUT.log
+status_check $?
 
 headings "remove default content of nginx"
 rm -rf /usr/share/nginx/html/* &>>OUTPUT.log
+status_check $?
 
 headings "Copy app contents in nginx"
 cp -r /tmp/frontend/dist/* /usr/share/nginx/html/ &>>OUTPUT.log
+status_check $?
 
 headings "Eanble nginx"
 systemctl enable nginx &>>OUTPUT.log
+status_check $?
 
 headings "Start nginx"
 systemctl start nginx &>>OUTPUT.log
+status_check $?
